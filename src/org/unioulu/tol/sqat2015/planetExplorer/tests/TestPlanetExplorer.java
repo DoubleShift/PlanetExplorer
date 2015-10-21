c1893eb7f7c905a58463b8ca0eb4d1aac0aa871f
somename <someemail>
2015-10-21 15:48:03 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -28,10 +28,10 @@ public class TestPlanetExplorer {
 		int[][] planet_array = new int[][]{
 		      {0,0,0,0,0,0},
 		      {0,0,0,0,0,0},
-		      {0,0,0,1,0,0},
+		      {0,0,1,0,0,0},
 		      {0,0,0,0,0,0},
 		      {0,0,0,0,0,0},
-		      {1,0,0,0,0,0}
+		      {0,0,0,0,0,1}
 		      
 		};
 		PlanetExplorer planet = new PlanetExplorer(6,6,"(5,5)(2,2)");
