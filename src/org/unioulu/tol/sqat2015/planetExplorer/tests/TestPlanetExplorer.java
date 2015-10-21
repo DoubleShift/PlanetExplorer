f007b2628a2a7b135cf14d0df9c0f482fb588f9b
somename <someemail>
2015-10-21 15:43:51 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -26,12 +26,12 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_create_obstacles(){
 		int[][] planet_array = new int[][]{
-		      {0,0,0,0,0,1},
 		      {0,0,0,0,0,0},
 		      {0,0,0,0,0,0},
-		      {0,0,1,0,0,0},
+		      {0,0,0,1,0,0},
 		      {0,0,0,0,0,0},
-		      {0,0,0,0,0,0}
+		      {0,0,0,0,0,0},
+		      {1,0,0,0,0,0}
 		      
 		};
 		PlanetExplorer planet = new PlanetExplorer(6,6,"(5,5)(2,2)");
